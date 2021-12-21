251b70f1189c4c151ea81814dae4818fac039ed2
shayan-hamidi <sha.hamidi10@gmail.com>
2021-12-21 14:31:43 +0330

    table-react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import UsersTable from "./UsersTable";
+function App() {
+  return (
+ <div>
+   <UsersTable />
+ </div>
+  );
+}
+
+export default App;
---
+++ src/UsersData.js
@@ -0,0 +1,6 @@
+const users = [
+    {id:1, fullName:'shayan hamidi', status:'developer',email:'sha.hamidi10@gmail.com'},
+    {id:2, fullName:'mohammad khani', status:'gamer',email:'mo.khani722@gmail.com'},
+    {id:3, fullName:'alireza hojjatpour', status:'sarbaz',email:'alireza.hojjatpour@gmail.com'},
+  ]
+  export default users;
